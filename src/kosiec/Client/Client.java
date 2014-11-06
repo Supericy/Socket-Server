f399bcd2ca6a341f3d98737fe377efe362fbd2cb
Supericy <koise150@hotmail.com>
2014-11-05 21:31:31 -0600

    refactoring, as well as client-server connect/disconnect handshake

--- src/kosiec/Client/Client.java
+++ src/kosiec/Client/Client.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,31 +15,113 @@ import java.util.Observable;
  */
 public class Client {
 
-	public static final String DISCONNECT_MESSAGE = "Disconnect";
+	public static final String DISCONNECT_COMMAND = "Disconnect";
+	public static final String DISCONNECTED_RESPONSE = "Disconnected";
 
-	private final Socket socket;
-	private final BufferedReader in;
-	private final PrintStream out;
+	private Socket socket;
+	private BufferedReader in;
+	private PrintStream out;
+	private String ip;
+	private int port;
 	private final List<ResponseListener> listeners;
+	private boolean connected;
+	private Thread listenerThread;
 
-	public Client(String ip, int port) throws IOException
+	public Client(String ip, int port)
 	{
-		this.socket = new Socket(ip, port);
-		this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		this.out = new PrintStream(socket.getOutputStream());
-
+		this.ip = ip;
+		this.port = port;
 		this.listeners = new ArrayList<ResponseListener>();
+		this.connected = false;
+		this.listenerThread = null;
+
+		addListener(new ResponseListener() {
+			@Override
+			public void handle(String response)
+			{
+				if (DISCONNECTED_RESPONSE.equals(response))
+					disconnect(false, false);
+			}
+		});
+	}
+
+	public void listen()
+	{
+		listenerThread = new Thread(new Runnable() {
+			@Override
+			public void run()
+			{
+				try
+				{
+					while (isConnected() && !Thread.interrupted())
+					{
+						emit(readMsg());
+					}
+				}
+				catch (IOException e)
+				{
+					System.err.println("Unexpected server shutdown");
+					disconnect(false, true);
+				}
+			}
+		});
+
+		listenerThread.setDaemon(true);
+		listenerThread.start();
+	}
+
+	public void connect() throws IOException
+	{
+		socket = new Socket();
+
+		socket.connect(new InetSocketAddress(ip, port), 2000);
+
+		in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		out = new PrintStream(socket.getOutputStream());
+
+		connected = true;
+	}
+
+	public void disconnect()
+	{
+		disconnect(true, false);
+	}
+
+	public void disconnect(boolean disconnectHandshake, boolean forceEmit)
+	{
+		try
+		{
+			if (disconnectHandshake)
+				sendMsg(DISCONNECT_COMMAND);
+
+			if (forceEmit)
+				emit(DISCONNECTED_RESPONSE);
+
+			in.close();
+			out.close();
+			socket.close();
+			socket = null;
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+
+		connected = false;
 	}
 
 	public boolean isConnected()
 	{
-		return socket.isConnected();
+		return socket != null && socket.isConnected() && connected;
 	}
 
 	public void sendMsg(String msg) throws IOException
 	{
 		if (!isConnected())
 			throw new IOException("socket closed");
+
+		if (msg == null)
+			throw new IOException("msg can't be null");
 
 		out.println(msg);
 	}
@@ -56,21 +139,15 @@ public class Client {
 		listeners.add(listener);
 	}
 
-	public void listen() throws IOException
+	public void emit(String msg)
 	{
-		if (!isConnected())
-			throw new IOException("socket closed");
-
-		String msg = readMsg();
-
-		if ("Disconnected".equals(msg))
+		if (msg != null)
 		{
-			socket.close();
-		}
-
-		for (ResponseListener listener : listeners)
-		{
-			listener.handle(msg);
+			for (ResponseListener listener : listeners)
+			{
+				listener.handle(msg);
+			}
 		}
 	}
+
 }
